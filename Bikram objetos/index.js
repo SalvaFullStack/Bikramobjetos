ccdb3dc1c1dee181b610ba8c108725a12e048001
SalvaFullStack <145378561+SalvaFullStack@users.noreply.github.com>
2023-09-27 18:30:00 +0200

    Add files via upload

---
+++ Bikram objetos/index.js
@@ -0,0 +1,85 @@
+const coche = {marca:"Toyota", modelo:"Auris", matricula: "4606 JPZ"}
+const casa = {codPostal: "41003", calle: "Girasol", portal "4", piso: "2"}
+const FullstackDeveloper = {array lenguajes, array proyectos}
+const Perro = {nombre "Fivi", raza: "bodeguero", color: "blanco", edad: "2", function ladrar() {console.log="guau"
+    
+}, function popo() {return Math.random() * 3;
+    
+} 
+
+const Portatil = {marca:"Lenovo", modelo:"Apiticaun"}; 
+let marcaPortatil = Portatil.marca; 
+console.log(marcaPortatil);
+
+const Portatil = {marca:"Lenovo", modelo:"Apiticaun"}; 
+let marcaPortatil2 = Portatil["marca"]; 
+console.log(marcaPortatil2); 
+
+const Concierto = {fecha: "2023", guitarras: "Suhr", array_grupos: ["Metallica", "Foo Fighters", "Dream Theater"]}; 
+let Grupos = Concierto.array_grupos; 
+console.log(Grupos); 
+
+const Led = {color1:"rojo", color2:"verde", color3:"azul"};
+let array_RGB = [Led.color1, Led.color2, Led.color3];
+console.log(array_RGB);
+
+const Portatil = {marca:"Lenovo", modelo:"Apiticaun"}; 
+let modeloPortatil = Portatil.modelo = 345;
+console.log(modeloPortatil)
+
+const Concierto = {fecha: "2023", cartelera: "Suhr", array_grupos: ["Metallica", "Foo Fighters", "Dream Theater"]}; 
+let carteleraConcierto = Concierto.cartelera = "Guns and roses"; 
+console.log(carteleraConcierto); 
+
+const Concierto = {fecha: "2023", guitarras: "Suhr", array_grupos: ["Metallica", "Foo Fighters", "Dream Theater"]}; 
+let fechaConcierto = Concierto.fecha = "26/09/2023"; 
+console.log(fechaConcierto);
+
+const Impresora = {imprimiendo:"OK", marca: "HP"};
+Impresora.imprimiendo = {nombreArchivo:"index", copias:"3", numPaginas:"100"};
+console.log(Impresora.imprimiendo);
+
+cont Noticia = {titular:"Sevilla FC gana 5-1", cuerpo: "Gol de Lukebakio"}; 
+const Persona = {nombre: "Salva", apellidos: "Nieto Redondo", edad: "43"}; 
+const Avion = {numPasajeros:"100", function despegar() {console.log("despegando")}, function aterrizar() {console.log("aterrizando")}; 
+
+const Paquete = {contenido: ["cepillo", "peine", "zapato"]}; 
+
+let Pais = {numHabitantes: "47 millones", continente: "Europa", gentilicio: "español"}; 
+
+const O_Error = {codigo:"RX"}; 
+codError = O_Error.codigo; 
+console.log(codError);
+
+const Grupo = {array_integrantes:["Antonio", "Salva", "Aitor", "Cirilo", "Juanma"]}; 
+let Integrantes = Grupo.array_integrantes; 
+console.log(Integrantes); 
+
+const Impresora = {imprimiendo:"OK", marca: "HP", array_tinta: ["rojo", "verde", "azul"]};
+let nivelesTinta = Impresora.array_tinta; 
+console.log(nivelesTinta); 
+
+const Pantalla = {tamaño:"grande", array_bidimensional_pixels: "1080px"}; 
+let pixeles = Pantalla.array_bidimensional_pixels; 
+console.log(pixeles); 
+
+const Movil = {especificaciones:"Complicado", estado:"Nuevo"}
+let especificaciones = Movil["especificaciones"]
+console.log(especificaciones); 
+
+const Grupo = {array_integrantes:["Antonio", "Salva", "Aitor", "Cirilo", "Juanma"], numIntegrantes:"3"};
+let nuevoGrupo = (Grupo.numIntegrantes = 5);
+console.log(nuevoGrupo);
+
+const Pantalla = {tamaño:"grande", array_bidimensional_pixels: "1080px", dimensiones: "1320x80"}; 
+let nuevaPantalla = (Pantalla.dimensiones = 1920x1080); 
+
+const Led = {encendido:true, color1:"rojo", color2:"verde", color3:"azul"};
+if (Led.encendido === true) {
+    Led.encendido = false
+} else { Led.encendido = true
+}
+
+const Movil = {especificaciones:"Complicado", estado:"Nuevo", temperatura: "30"}
+let nuevaTemperatura = (Movil.temperatura = "20");
+console.log(nuevaTemperatura);
